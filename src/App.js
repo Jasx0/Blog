7c603b53dbd666c5c64789f6e1347249222e7d62
KarthikRaja <karthikrajaveluchamy2001@gmail.com>
2023-06-21 18:46:03 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import TopBar from "./components/topbar/TopBar"
+import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { useContext } from "react";
+import { Context } from "./components/context/Context";
+
+
+function App() {
+
+  const user = useContext(Context);//change the code false
+  return (
+    <Router>
+      <div>
+        <TopBar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={ user ? <Home />: <Register />} />
+          <Route path="/login" element={ user ? <Home/>:<Login />} />
+          <Route path="/write" element={user ? <Write/> :<Register/>} />
+          <Route path="/settings" element={user ? <Settings />:<Register/>} />
+          <Route path="/post/:id" element={<Single />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+     
+     
+     
+
+   
+
+export default App;
+
+
+
